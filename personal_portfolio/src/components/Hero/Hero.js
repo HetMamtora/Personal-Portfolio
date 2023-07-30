a4a5c92b7f0d2c1f94806ff402b2a85987a8f395
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-30 18:55:13 +0530

    Update Hero.js

--- personal_portfolio/src/components/Hero/Hero.js
+++ personal_portfolio/src/components/Hero/Hero.js
@@ -8,13 +8,12 @@ const Hero = (props) => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
-        HET MAMTORA <br /><br />
+        Your Name <br /><br />
         Welcome To <br />
         My Personal Portfolio
       </SectionTitle>
       <SectionText>
         ReactJS Developer | Front-End Developer | Back-End Deveoper <br />
-        Recent graduate from Nirma University in May'23, majored in Computer Science Engineering.
       </SectionText>
       <Button onClick={() => window.location = 'mailto:mamtorahet@yahoo.com'}>Email</Button>
     </LeftSection>
