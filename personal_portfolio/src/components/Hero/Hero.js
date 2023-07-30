73706f41abb7ff774becc9721230e37be4997122
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-30 14:31:04 +0530

    updated

--- personal_portfolio/src/components/Hero/Hero.js
+++ personal_portfolio/src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Full Stacl Developer | ReactJS Developer | Front End Developer
+      </SectionText>
+      <Button onClick={() => window.location = 'mailto:mamtorahet@yahoo.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
