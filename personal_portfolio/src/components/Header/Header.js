7b3cb8153623a60fd267f92c211f16260e290a4c
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-30 14:14:28 +0530

    updated

--- personal_portfolio/src/components/Header/Header.js
+++ personal_portfolio/src/components/Header/Header.js
@@ -6,9 +6,43 @@ import { DiCssdeck } from 'react-icons/di';
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href='/' legacyBehavior>
+        <a style={{display:'flex', alignItems: 'center', color:'white'}}>
+          <DiCssdeck size='3rem' /> <span> Portfolio </span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href='#project' legacyBehavior>
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#tech' legacyBehavior>
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#about' legacyBehavior>
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href='https://github.com'>
+        <AiFillGithub size='3rem'/>
+      </SocialIcons>
+      <SocialIcons href='https://github.com'>
+        <AiFillLinkedin size='3rem'/>
+      </SocialIcons>
+      <SocialIcons href='https://github.com'>
+        <AiFillInstagram size='3rem'/>
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
