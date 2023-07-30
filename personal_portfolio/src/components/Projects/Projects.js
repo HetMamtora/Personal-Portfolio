321339ff69aa2db44b0ef554663f8293f7545967
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-30 15:02:06 +0530

    updated

--- personal_portfolio/src/components/Projects/Projects.js
+++ personal_portfolio/src/components/Projects/Projects.js
@@ -5,18 +5,34 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <Section nopadding  id='projects'>
+  <Section nopadding id="projects">
     <SectionDivider />
-    <SectionTitle main> Projects </SectionTitle>
+    <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {projects.map(({id, image, title, description, tags, source, visit}) => (
-        <BlogCard key={id}>
-          <img src={image} />
-          <TitleContent>
-            <HeaderThree title>{title}</HeaderThree>
-          </TitleContent>
-        </BlogCard>
-      ))}
+      {projects.map((p, i) => {
+        return (
+          <BlogCard key={i}>
+          <Img src={p.image} />
+            <TitleContent>
+              <HeaderThree title>{p.title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo className="card-info">{p.description}</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {p.tags.map((t, i) => {
+                  return <Tag key={i}>{t}</Tag>;
+                })}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={p.visit}>Code</ExternalLinks>
+              <ExternalLinks href={p.source}>Source</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        );
+      })}
     </GridContainer>
   </Section>
 );
