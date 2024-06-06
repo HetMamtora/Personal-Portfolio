b49b2055889dbc57d6adde38562347dd9770b5fa
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-06-06 19:57:26 +0530

    updated

--- personal_portfolio/src/components/Acomplishments/Acomplishments.js
+++ personal_portfolio/src/components/Acomplishments/Acomplishments.js
@@ -9,6 +9,10 @@ const data = [
   { number: 20, text: 'GitHub Repositories', },
   { number: 2, text: 'years of experience in Full Stack over personalized projects', }
 ];
+const data1 = [
+  { title: 'Learn Java Programming', text: 'Certificate](https://www.udemy.com/certificate/UC-ddf287c4-e89a-46e8-8a9b-08b2750e1b93/)' },
+  { title: '100DaysOfCode', text: `[Track](https://github.com/HetMamtora)` },
+];
 
 const Acomplishments = () => (
   <Section>
@@ -17,6 +21,15 @@ const Acomplishments = () => (
       {data.map((card, index) => (
         <Box key={index}>
           <BoxNum>{card.number}+</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+    
+    <Boxes>
+      {data1.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{card.title}</BoxNum>
           <BoxText>{card.text}</BoxText>
         </Box>
       ))}
